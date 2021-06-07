04358b9a32b8d3cabda777eb4113d38d0e0ec0e0
= <=>
2021-06-07 09:05:24 +0300

    Exercise 1

---
+++ Exercise1.py
@@ -0,0 +1,13 @@
+'''
+1. Write a Python program to convert JSON data to Python object. 
+
+'''
+
+import json
+
+json_obj =  '{ "Name":"Ali", "Class":"12", "Age":17 }'
+
+python_obj = json.loads(json_obj)
+
+for key,value in python_obj.items():
+    print(f"{key} = {value}")
