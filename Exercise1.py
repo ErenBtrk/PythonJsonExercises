01931f9fea133dbde818af742c5a94daa0521070
= <=>
2021-06-07 09:09:34 +0300

    Exercise 1

--- Exercise1.py
+++ Exercise1.py
@@ -8,6 +8,7 @@ import json
 json_obj =  '{ "Name":"Ali", "Class":"12", "Age":17 }'
 
 python_obj = json.loads(json_obj)
+print(type(python_obj))
 
 for key,value in python_obj.items():
     print(f"{key} = {value}")
